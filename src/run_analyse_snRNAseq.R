c87466813d6f735a89be0f638d76fcf8a1dc59ed
Alexandra Tidd <alexandrarubytidd@gmail.com>
2023-02-17 15:33:07 +0000

    add scripts to iteratively run analyse_snRNAseq on all runs with all params - add sbatch scripts

---
+++ src/run_analyse_snRNAseq.R
@@ -0,0 +1,28 @@
+args <- commandArgs(trailingOnly=TRUE)
+experiment <- args[1]
+genome <- args[2]
+sublibrary <- args[3]
+parse_analysis_subdir <- args[4]
+do_integration <- args[5]
+
+library(devtools) ; load_all()
+base_dir <- ifelse(Sys.info()["nodename"] == "Alexs-MacBook-Air-2.local",
+                   "/Volumes/TracerX/",
+                   "/camp/project/tracerX/")
+
+# testing: setwd # base_dir=ifelse(Sys.info()["nodename"]=="Alexs-MacBook-Air-2.local","/Volumes/TracerX/","/camp/project/tracerX/");setwd(paste0(base_dir,"working/VHL_GERMLINE/tidda/vhl/"));library(devtools);load_all();parse_dir=paste0(base_dir,"working/VHL_GERMLINE/tidda/parse_pipeline/");genome="hg38";sublibrary="comb";parse_analysis_subdir="all-well/DGE_unfiltered/";do_filtering=T;remove_doublets=T;min_nuclei_per_gene=NULL;min_nFeature_RNA=NULL;max_nCount_RNA=NULL;max_nFeature_RNA=NULL;max_percent_mito=NULL;n_dims=NULL;clustering_resolutions = seq(0.1, 0.8, by = 0.1);final_clustering_resolution=NULL;out_dir = NULL;sample_subset = NULL;do_timestamp = F;do_integration = F;integration_col="sample";
+# testing: pilot # experiment="221202_A01366_0326_AHHTTWDMXY";genome="hg38";sublibrary="SHE5052A9_S101";parse_analysis_subdir="all-well/DGE_filtered";do_integration=F
+# testing: full  # experiment="230127_A01366_0343_AHGNCVDMXY";genome="hg38";sublibrary="SHE5052A11_S164";parse_analysis_subdir="all-well/DGE_filtered";do_integration=F
+
+analyse_snRNAseq(
+  parse_dir = paste0(base_dir, "working/VHL_GERMLINE/tidda/parse_pipeline/"),
+  experiment = experiment,
+  genome = genome,
+  sublibrary = sublibrary,
+  parse_analysis_subdir = parse_analysis_subdir,
+  remove_doublets = F,
+  do_filtering = T,
+  min_nuclei_per_gene = 5,
+  final_clustering_resolution = 0.3,
+  do_integration = do_integration)
+
---
+++ src/submit_analyse_snRNAseq.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# create and submit the split-pipe comb sbatch script for each experiment / genome
+run_info=analyse_snRNAseq_${experiment}_${genome}_${sublibrary}_${parse_analysis_subdir/\//_}_int${do_integration}
+script=src/sbatch/sbatch_${run_info}.sh
+echo $run_info
+
+cat << EOF > $script
+#!/bin/bash
+#SBATCH --job-name=parse_${run_info}
+#SBATCH --time=1-00:00:0
+#SBATCH --mem=50GB
+#SBATCH -o /camp/project/tracerX/working/VHL_GERMLINE/tidda/vhl/log/${run_info}.out
+#SBATCH -e /camp/project/tracerX/working/VHL_GERMLINE/tidda/vhl/log/${run_info}.err
+
+echo "------------------------------"
+echo "Experiment: $experiment"
+echo "Genome: $genome"
+echo "Sublibrary: $sublibrary"
+echo "Parse analysis subdirectory: $parse_analysis_subdir"
+echo "Integration: $do_integration"
+echo "Script: $script"
+echo "------------------------------"
+
+# codify params
+experiment=$experiment
+genome=$genome
+sublibrary=$sublibrary
+parse_analysis_subdir=$parse_analysis_subdir
+
+# get directories
+cd $(pwd) ; . src/config.sh
+
+# run generate_QC_report.R
+Rscript src/run_analyse_snRNAseq.R \
+  $experiment \
+  $genome \
+  $sublibrary \
+  $parse_analysis_subdir \
+  $do_integration
+EOF
+
+# submit the script
+sbatch $script
