d4d9fd84cb61c510cedf33ecec7d229de51a8ebf
Alexandra Tidd <alexandrarubytidd@gmail.com>
2023-03-17 15:12:37 +0000

    add 221202 final_cluster_annotations to sysdata for testing

--- data-raw/sysdata.R
+++ data-raw/sysdata.R
@@ -18,10 +18,23 @@ umap_void_theme <-
                  legend.position = "none",
                  plot.margin = ggplot2::unit(c(2,2,2,2), "pt"))
 
+# final cluster annotations
+final_cluster_annotations <- list(
+  "221202_A01366_0326_AHHTTWDMXY" = list(
+    "1"="tumour",
+    "2"="tumour",
+    "3"="immune (monocytes)",
+    "4"="tumour",
+    "5"="endothelial",
+    "6"="immune (T cells)"
+  )
+)
+
 # save to sysdata
 usethis::use_data(transcript_types,
                   markers,
                   ditto_colours,
                   umap_void_theme,
+                  final_cluster_annotations,
                   overwrite = TRUE,
                   internal = TRUE)
